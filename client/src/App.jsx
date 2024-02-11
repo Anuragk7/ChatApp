8f12fd72c93adbdfe2655f0ce49c449d41d20e66
Anurag Khugshal <113304552+Anuragk7@users.noreply.github.com>
2024-02-11 20:29:18 +0530

    url syncing for hosting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { UserContextProvider } from "./assets/Usercontext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = 'http://localhost:4040';
+  axios.defaults.baseURL = 'https://chat-app-beta-five-23.vercel.app/';
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
